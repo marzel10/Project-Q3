11a0f9a2199df4c8df18ccaa28eca407225cc837
Maria <maria.zelewska@gmial.com>
2024-11-19 17:37:35 +0100

    position matrix added

--- Fastener design.py
+++ Fastener design.py
@@ -31,6 +31,23 @@ def cg_position(A):
 
     return x_cg,z_cg #return position of the center of gravity
 
+def position_matrix(zs,xs,col,c,D,H,n):
+    if zs<c*D:
+        zs=c*D
+    if xs<c*D:
+        xs=c*D
+    l=(n-1)*zs #lenght of the fasteners line
+    z1=np.arange(-l/2,l/2+zs,zs) #fasteners will be placed symetrically from 0 axis, +zs included to include the end
+    z2=np.arange(-l/2,l/2+zs,zs)
+    z=np.concatenate((z1,z2),axis=None)
+    xn=np.arange(-(H/2-1.5*D),-(H/2-1.5*D)+(col/2-1)*xs+xs,xs)
+    xp=np.arange((H/2-1.5*D)-(col/2-1)*xs,(H/2-1.5*D)+xs,xs)
+    x=np.concatenate((xn,xp,xp,xn),axis=None)
+    A=np.concatenate((x.reshape(-1,1),z.reshape(-1,1)),axis=1)
+    return A
+
 A=[[1,1,2],[-1,-1,1]]
 
 print(cg_position(A))
+print(number_of_fastners(2,11,"metal"))
+print(position_matrix(0,0,2,2.5,2,50,number_of_fastners(2,11,"metal")))
