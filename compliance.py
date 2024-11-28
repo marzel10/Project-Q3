80beb3c65d1e5e6e1ae4e54652fefa56c6d197c8
Maria <maria.zelewska@gmial.com>
2024-11-28 14:58:12 +0100

    Initial commit

---
+++ compliance.py
@@ -0,0 +1,48 @@
+from geometry_design import geometry_design
+import numpy as np
+def compliance_b(d_nom, d_minor,d_sha, E_b, E_n,ht,t_1,t_2):
+    #d_nom - nominal diameter of the fastener (from the standard specs)
+    #d_minor - minor diameter of the fastener (from the standard specs)
+    #d_sha - diamter of the shank (from the standard specs)
+    #t1 - thickness of the lug (from previous design steps)
+    #t2 - thickness of the spacecraft wall (from prevoius work packedges*)
+    #E_b, E_n - youngs modulus of the bolt and nut
+    #ht - head type (Hexagon or cylindrical)
+    #st - shank type (nut tightened or threaded hole)
+
+    if ht=="Hexagon":
+        Lh=0.5*d_nom
+    else:
+        Lh=0.4*d_nom
+
+
+    Ls=0.4*d_nom
+
+    Ls=0.33*d_nom
+
+    Ln=0.4*d_nom
+    A_nom=np.pi*d_nom**2/4
+    A_min=np.pi*d_minor**2/4
+    A_sha=np.pi*d_sha**2/4
+
+    com_b=(Lh/A_nom+Ls/A_min+(t_1+t_2)/A_sha)/E_b+Ln/E_n/A_nom
+
+    return com_b
+
+def compliance_a(t,D_out,D_in,E_a):
+    #t thickness of the plate
+    #D_out diameter of the head
+    #D_in diameter of the shank
+    #E_a young modulus of the attached part (lug plate or the s/c wall)
+    com_a=4*t/(E_a*np.pi*(D_out**2-D_in**2))
+
+    return com_a
+
+def force_ratio(d_nom, d_minor,d_sha, E_b, E_n,ht,t_1,D_out,E_p,t_2,E_w):
+    # t_1 - thickness of the lug (from previous design steps)
+    # t_2 - thickness of the spacecraft wall (from prevoius work packedges*)
+
+    fr=(compliance_a(t_1,D_out,d_sha,E_p)+compliance_a(t_2,D_out,d_sha,E_w))/(compliance_a(t_1,D_out,d_sha,E_p)+compliance_a(t_2,D_out,d_sha,E_w)+compliance_b(d_nom,d_minor,d_sha, E_b, E_n,ht,t_1,t_2))
+
+    return fr
+
